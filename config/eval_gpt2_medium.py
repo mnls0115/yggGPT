38d8333d014b9bd39674a4d37ccb0395fdca5664
이태권 <84170342+mnls0115@users.noreply.github.com>
2024-04-14 21:53:18 +0900

    Update eval_gpt2_medium.py

--- config/eval_gpt2_medium.py
+++ config/eval_gpt2_medium.py
@@ -1,9 +1,9 @@
 # evaluate the base gpt2
-# n_layer=36, n_head=20, n_embd=1280
-# 774M parameters
+# n_layer=24, n_head=16, n_embd=1024
+# 350M parameters
 batch_size = 8
 eval_iters = 500 # use more iterations to get good estimate
 eval_only = True
 wandb_log = False
-init_from = 'gpt2-large'
+init_from = 'gpt2-medium'
 
